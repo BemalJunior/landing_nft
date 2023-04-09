b21258fc07d0510c06969a8dcdec5cfe3346832c
BemalJunior <bimal-c163@yandex.ru>
2023-04-09 22:33:57 +0400

    accordion end

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let btn = document.querySelector('#btn');
-let span = document.querySelector('.span');
-let qa__wrapper = document.querySelector('.qa__wrapper');
+// let btn = document.querySelector('#btn');
+// let span = document.querySelector('.span');
+let qa__wrapper = document.querySelectorAll('.qa__wrapper');
 
+qa__wrapper.forEach(el => {
+  el.addEventListener('click', () => {
+    let span = el.childNodes[5]
+    let btn = el.childNodes[3]
+    let qa__wrapper = el
 
-qa__wrapper.addEventListener('click', () => {
-  if (span.style.maxHeight === `0px`) {
+      if (span.style.maxHeight === `0px`) {
     span.style.maxHeight = span.scrollHeight + "px";
     qa__wrapper.style.background = "#fff";
     btn.style.transform = "rotate(180deg)"
@@ -13,4 +17,19 @@ qa__wrapper.addEventListener('click', () => {
     qa__wrapper.style.background = "#fcfaff";
     btn.style.transform = "rotate(0deg)";
   }
+
+  })
 })
+
+
+// qa__wrapper.addEventListener('click', () => {
+//   if (span.style.maxHeight === `0px`) {
+//     span.style.maxHeight = span.scrollHeight + "px";
+//     qa__wrapper.style.background = "#fff";
+//     btn.style.transform = "rotate(180deg)"
+//   } else {
+//     span.style.maxHeight = 0;
+//     qa__wrapper.style.background = "#fcfaff";
+//     btn.style.transform = "rotate(0deg)";
+//   }
+// })
